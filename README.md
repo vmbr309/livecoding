dfb2b6525422e9153f376d7bf0e200f885c9012d
vmbr309 <154901438+vmbr309@users.noreply.github.com>
2025-06-07 17:46:36 -0300

    folders + manic

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # livecoding
 *Created on 2025-06-05*
 
-A repository to store my files pertaining to live coding music in Sonic Pi (Ruby) and Strudel/TidalCycles (Haskell).
+A repository to store my files pertaining to live coding music in Sonic Pi (Ruby) and Strudel/TidalCycles (JS/Heskell).
---
+++ json/maniacV1.js
@@ -0,0 +1,10 @@
+// "Maniac" Test -- vmbr
+
+   ([hh hh hh hh] [hh hh hh hh] [hh hh hh hh] [hh hh hh hh],
+   [- - - -][rim - - -] [- - - -] [rim rim rim -],
+   [- - - -] [- - - -] [- - - cp*4] [cp cp*4 cp -],
+   [cb cb*3 cb -] [- - - -] [- - - -] [- - - cb*3],
+   [- - sd - ]  [- - sd -]   [- - sd - ] [- - sd -],
+   [- - sd - ]  [- - sd -]   [- - sd - ] [- - sd -],
+   [bd - bd - ] [bd - bd - ] [bd - bd -] [bd - bd -]
+   `).bank("linndrum").cpm(77/4)
