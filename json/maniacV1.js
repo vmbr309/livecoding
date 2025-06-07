75ce111cde9a688f9ee7353be3e90fe6e33449ae
vmbr309 <154901438+vmbr309@users.noreply.github.com>
2025-06-07 18:42:32 -0300

    manic ok

--- json/maniacV1.js
+++ json/maniacV1.js
@@ -1,10 +1,26 @@
-// "Maniac" Test -- vmbr
+// "Maniac" Test — vmbr
 
-   ([hh hh hh hh] [hh hh hh hh] [hh hh hh hh] [hh hh hh hh],
-   [- - - -][rim - - -] [- - - -] [rim rim rim -],
-   [- - - -] [- - - -] [- - - cp*4] [cp cp*4 cp -],
-   [cb cb*3 cb -] [- - - -] [- - - -] [- - - cb*3],
-   [- - sd - ]  [- - sd -]   [- - sd - ] [- - sd -],
-   [- - sd - ]  [- - sd -]   [- - sd - ] [- - sd -],
-   [bd - bd - ] [bd - bd - ] [bd - bd -] [bd - bd -]
-   `).bank("linndrum").cpm(77/4)
+let linn = `
+[hh hh hh hh] [hh hh hh hh] [hh hh hh hh] [hh hh hh hh],
+[- - - -][rim - - -] [- - - -] [rim rim rim -],
+[- - - -] [- - - -] [- - - cp*4] [cp cp*4 cp -],
+[cb cb*3 cb -] [- - - -] [- - - -] [- - - cb*3],
+[- - sd - ] [- - sd -] [- - sd - ] [- - sd -],
+[bd - bd - ] [bd - bd - ] [bd - bd -] [bd - bd -]
+`;
+
+let sds5 = `
+[- - - - ] [- - - ] [- - - -] [- ht mt lt],
+[- - sd - ] [- - sd -] [- - sd -] [- - sd sd]
+`;
+
+let clap = `
+[- - cp - ] [- - cp -] [- - cp -] [- - cp -],
+[- - sd - ] [- - sd -] [- - sd -] [- - sd -]
+`;
+
+$: stack(
+  sound(linn).bank("Linndrum"),
+  sound(sds5).bank("SimmonsSDS5"), 
+  sound(clap).bank("RolandTR909").gain(1.3)
+).cpm(77/4)
